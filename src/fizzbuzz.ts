11bb2b51000da91d114c1feeedc35d115452783f
maxime <maxime@maximes-MacBook-Pro.local>
2023-03-03 17:22:55 +0100

    :sparkles: add solution

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,1 +1,26 @@
-export const fizzBuzz = () => undefined;
+export function fizzBuzz(nbr: number): string {
+  if (isDivisibleBy3(nbr) && isDivisibleBy5(nbr)) {
+    return "FizzBuzz";
+  }
+
+  if (isDivisibleBy3(nbr)) {
+    return "Fizz";
+  }
+
+  if (isDivisibleBy5(nbr)) {
+    return "Buzz";
+  }
+  return nbr.toString();
+}
+
+function isDivisibleBy(nbr: number, divisor: number): boolean {
+  return nbr % divisor === 0;
+}
+
+function isDivisibleBy3(nbr: number): boolean {
+  return isDivisibleBy(nbr, 3);
+}
+
+function isDivisibleBy5(nbr: number): boolean {
+  return isDivisibleBy(nbr, 5);
+}
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -1,7 +1,32 @@
-import { fizzBuzz } from '../src/fizzbuzz'
+import { fizzBuzz } from "../src/fizzbuzz";
 
-describe('FizzBuzz', () => {
-    it('example test', () => {
-        expect(fizzBuzz()).toEqual(undefined)
-    })
-})
+describe("FizzBuzz", () => {
+  it("if number is 1 then return 1", () => {
+    expect(fizzBuzz(1)).toEqual("1");
+  });
+
+  it("if number is 2 then return 2", () => {
+    expect(fizzBuzz(2)).toEqual("2");
+  });
+
+  it("if number is 3 then return Fizz", () => {
+    expect(fizzBuzz(3)).toEqual("Fizz");
+  });
+
+  it("if number is 6 then return Fizz", () => {
+    expect(fizzBuzz(6)).toEqual("Fizz");
+  });
+
+  it("if number is 5 then return Buzz", () => {
+    expect(fizzBuzz(5)).toEqual("Buzz");
+  });
+
+  it("if number is 10 then return Buzz", () => {
+    expect(fizzBuzz(10)).toEqual("Buzz");
+  });
+
+  it("if number is 15 then return FizzBuzz", () => {
+    expect(fizzBuzz(15)).toEqual("FizzBuzz");
+  });
+  
+});
